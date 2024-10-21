c79514ba3d0810c30af972ea5273d69ecaa4e947
GrahamSH <grahamshllk@gmail.com>
2024-10-21 22:58:35 +0000

    format and fix bug

--- pwa/app/routes/apidocs.$doc.($version).tsx
+++ pwa/app/routes/apidocs.$doc.($version).tsx
@@ -1,8 +1,49 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
-import { ClientLoaderFunctionArgs, Params, json, useLoaderData } from '@remix-run/react';
+import {
+  ClientLoaderFunctionArgs,
+  Params,
+  json,
+  useLoaderData,
+} from '@remix-run/react';
+import { useNavigate } from '@remix-run/react';
 import { ApiReferenceReact } from '@scalar/api-reference-react';
 import '@scalar/api-reference-react/style.css';
+import { useEffect, useRef } from 'react';
 
+function useDelegatedReactRouterLinks(nodeRef: React.RefObject<HTMLElement>) {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    let node = nodeRef.current;
+    let handler = (event: MouseEvent) => {
+      if (!nodeRef.current) return;
+
+      if (!(event.target instanceof HTMLElement)) return;
+
+      let a = event.target.closest('a');
+
+      if (
+        a && // is anchor or has anchor parent
+        a.hasAttribute('href') && // has an href
+        a.host === window.location.host && // is internal
+        event.button === 0 && // left click
+        (!a.target || a.target === '_self') && // Let browser handle "target=_blank" etc.
+        !(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey) // not modified
+      ) {
+        event.preventDefault();
+        let { pathname, search, hash } = a;
+        navigate({ pathname, search, hash });
+      }
+    };
+
+    if (!node) return;
+    node.addEventListener('click', handler);
+
+    return () => {
+      node?.removeEventListener('click', handler);
+    };
+  }, [navigate, nodeRef]);
+}
 async function loadData(params: Params) {
   if (!params.doc) {
     throw new Response(null, {
@@ -28,16 +69,22 @@ export async function clientLoader({ params }: ClientLoaderFunctionArgs) {
 
 export default function ApiDocsV3(): React.JSX.Element {
   const { path } = useLoaderData<typeof loader>();
+  let ref = useRef<HTMLDivElement>(null);
+
+  useDelegatedReactRouterLinks(ref);
+
   return (
     <>
-      <ApiReferenceReact
-        configuration={{
-          hideDarkModeToggle: true,
-          spec: {
-            url: path,
-          },
-        }}
-      />
+      <div ref={ref}>
+        <ApiReferenceReact
+          configuration={{
+            hideDarkModeToggle: true,
+            spec: {
+              url: path,
+            },
+          }}
+        />
+      </div>
     </>
   );
 }
